7e3ea4d04821e39269330528bb4d65cdd0cf0363
notdraimdev <notdraimdev@gmail.com>
2024-09-15 14:13:41 +0200

    Linux fixes

--- scenes/LinuxDisclaimer.gd
+++ scenes/LinuxDisclaimer.gd
@@ -1,9 +1,15 @@
 extends AspectRatioContainer
+@onready var label: RichTextLabel = $VBoxContainer/Label
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	await draw
+	var file = FileAccess.open("user://data.dat", FileAccess.READ)
+	print(file.get_path_absolute())
+	label.text = label.text.replace(".DIR.",file.get_path_absolute().replace("data.dat",""))
+	DisplayServer.clipboard_set(file.get_path_absolute().replace("data.dat",""))
+	OS.shell_open("https://ffmpeg.org/download.html#build-linux")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
